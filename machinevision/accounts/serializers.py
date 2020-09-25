636756678fd62dc70c266a9a79b274f3165f4eb3
BadassHenkka <henri.kestio@gmail.com>
2020-09-25 15:41:01 +0300

    Add api endpoints for authentication, register, login, fetching user and logout

---
+++ machinevision/accounts/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'],
+            validated_data['email'],
+            validated_data['password'],
+            # User has to be activated by admin
+            is_active=False,
+        )
+
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        initial_user_data = User.objects.filter(
+            username=self.initial_data['username']).first()
+
+        if hasattr(initial_user_data, 'is_active') and not initial_user_data.is_active:
+            raise serializers.ValidationError(
+                "User not active. Request access from site admin.")
+
+        user = authenticate(**data)
+
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
