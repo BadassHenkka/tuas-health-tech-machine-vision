4329722b5251927dc3e82c04ca591bb2623d8096
BadassHenkka <henri.kestio@gmail.com>
2020-09-26 15:33:23 +0300

    Add Login and Register components and routing to the components using react-router-dom

--- machinevision/frontend/src/App.js
+++ machinevision/frontend/src/App.js
@@ -1,22 +1,42 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 import HeaderMenuBar from './components/layout/HeaderMenuBar';
+import Login from './components/accounts/Login';
+import Register from './components/accounts/Register';
+
+const Dashboard = () => {
+  return (
+    <Typography
+      component='div'
+      style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
+    ></Typography>
+  );
+};
 
 const App = () => {
   return (
-    <Fragment>
-      <HeaderMenuBar />
-      <Container>
-        <Typography
-          component='div'
-          style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
-        />
-      </Container>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <HeaderMenuBar />
+        <Container>
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </Container>
+      </Fragment>
+    </Router>
   );
 };
 
